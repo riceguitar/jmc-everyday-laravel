585c6ffbaf2ac97e0bc62a5b515c518abac8304a
David Sudarma <david@26am.com>
2016-08-03 00:26:43 -0700

    Public Facing page

---
+++ html/index.php
@@ -0,0 +1,42 @@
+<?php include('partials/_header.php'); ?>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<p>This digital online course
+				with John C. Maxwell is your
+				guide to experiencing your
+				most fulfilling life.</p>
+
+				<p>
+				<a href="" class="btn btn-default">Buy Now</a>
+				<a href="" class="btn btn-default">More Info</a>
+				</p>
+
+			</div>
+			<div class="col-sm-6">
+				<!-- Image of John -->
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-sm-3">Col 1</div>
+					<div class="col-sm-3">Col 2</div>
+					<div class="col-sm-3">Col 3</div>
+					<div class="col-sm-3">Col 4</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				Right Side
+			</div>
+		</div>
+	</section>
+</section>
+
+<?php include('partials/_footer.php'); ?>
---
+++ html/partials/_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Living With Purpose</title>
+    <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+  </head>
+
+  <body>
+    <header id="global-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            Logo
+          </div>
+          <div class="col-sm-6">
+            Menu
+          </div>
+        </div>
+      </div>
+    </header>
